a5830d8d4897103105193e0600929bdf3d2aabeb
Mohammad Parsaie <mpa87@sfu.ca>
2023-02-19 12:27:14 -0800

    Started input classes, yet incompelete

--- src/main/java/group7/main/GamePanel.java
+++ src/main/java/group7/main/GamePanel.java
@@ -1,10 +1,19 @@
 package group7.main;
 
+import group7.inputs.*;
+
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * is responsible for ???
+ * @author Salman Ayaz, Karmen Yung, Mohammad Parsaei, Chen Min
+ */
 public class GamePanel extends JPanel {
-
+    public GamePanel(){
+        super.addKeyListener(new KeyboardInputs());
+        super.addMouseListener(new MouseInputs());
+    }
     public void paint(Graphics g){
         g.drawRect(100,100,50,50);
     }
--- src/main/java/group7/main/GameWindow.java
+++ src/main/java/group7/main/GameWindow.java
@@ -2,10 +2,24 @@ package group7.main;
 
 import javax.swing.*;
 
+/**
+ * Sets up the window frame for Game.
+ * @author Salman Ayaz, Karmen Yung, Mohammad Parsaei, Chen Min
+ */
 public class GameWindow {
+
     private JFrame jframe;
+
+    /**
+     * Initializes jframe field and changes jframe's attributes
+     * @param gamePanel
+     */
     public GameWindow(GamePanel gamePanel){
         jframe= new JFrame();
+        // changing attributes of window by calling changeWindowAttributes field
+        changeWindowAttributes(gamePanel);
+    }
+    private void changeWindowAttributes(GamePanel gamePanel){
         // Terminate the program when player closes the window
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // window size set to be 1280x720
