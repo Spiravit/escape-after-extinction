194f9ed145982e9052aae1311bd403919c9bd1fb
Salman Ayaz <salmanayazz@gmail.com>
2023-02-21 18:59:43 -0800

    refactored entity classes

--- src/main/java/group7/entities/Inanimate.java
+++ src/main/java/group7/entities/Inanimate.java
@@ -2,6 +2,6 @@ package group7.entities;
 
 public abstract class Inanimate extends Entity {
     public Inanimate(double positionX, double positionY, double height, double width) {
-        super(positionX, positionY, height, width);
+        super(positionX, positionY);
     }
 }
