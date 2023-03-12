f848c9e2c7ee547c7a65831d78e8bd32aab0873d
karmeny <karmeny1105@hotmail.com>
2023-03-11 21:07:57 -0800

    added level 2 and 3 maps

--- src/main/java/group7/Game.java
+++ src/main/java/group7/Game.java
@@ -2,13 +2,13 @@ package group7;
 
 import group7.Graphics.GraphicsPanel;
 import group7.Graphics.GraphicsWindow;
-import group7.entities.animate.Player;
 import group7.Graphics.GraphicsGrid;
 import group7.gameStates.*;
-import group7.levels.Pathfinding;
-import group7.utils.AssetLoader;
-import group7.levels.Level;
-import group7.levels.LevelManager;
+//import group7.entities.animate.Player;
+//import group7.levels.Pathfinding;
+//import group7.utils.AssetLoader;
+//import group7.levels.Level;
+//import group7.levels.LevelManager;
 
 
 public class Game implements Runnable {
@@ -19,7 +19,6 @@ public class Game implements Runnable {
     // Two different states of game
     private InLevelState inLevelState;
     private  MainMenuState mainMenuState;
-
 
     private GraphicsGrid graphicsGrid;
     public gameStates gameCurrentState; // The current running state of the game
@@ -53,15 +52,15 @@ public class Game implements Runnable {
 
     public void changeGameStates(gameStates gameStateParameter){
         // changing the gameCurrentState to indicate the current running state of game in game class
-        if (gameStateParameter==gameStates.IN_LEVEL && gameCurrentState==gameStates.IN_LEVEL){
-            if (inLevelState.isPaused==true){
-                inLevelState.isPaused=false;
+        if ( gameStateParameter == gameStates.IN_LEVEL && gameCurrentState == gameStates.IN_LEVEL ) {
+            if ( inLevelState.isPaused == true ) {
+                inLevelState.isPaused = false;
             }
         }
-        if (gameCurrentState == gameStateParameter ){
+        if ( gameCurrentState == gameStateParameter ){
             return;
         }
-        if (gameStateParameter == gameStates.IN_MENU ) {
+        if ( gameStateParameter == gameStates.IN_MENU ) {
             // changing the gameStates field in graphicsPanel so that
             // the graphicsPanel will use the rendering methods of current running state to render the game
             // Here, once the game state is changed to main menu in game class,
@@ -72,49 +71,49 @@ public class Game implements Runnable {
             playerSelectFlag = false;
             levelSelectFlag = false;
         }
-        if (gameStateParameter == gameStates.CREDIT_SUB_MENU ) {
+        if ( gameStateParameter == gameStates.CREDIT_SUB_MENU ) {
             this.gameCurrentState = gameStateParameter;
             mainMenuState = new creditState(this);
             graphicsPanel.changeGameStates(mainMenuState);
         }
-        if (gameStateParameter == gameStates.PLAYER_SELECTION_SUB_MENU ) {
+        if ( gameStateParameter == gameStates.PLAYER_SELECTION_SUB_MENU ) {
             this.gameCurrentState = gameStateParameter;
             mainMenuState = new playerSelectionState(this);
             graphicsPanel.changeGameStates(mainMenuState);
             playerSelectFlag = true;
         }
-        if (gameStateParameter == gameStates.LEVEL_SELECTION_SUB_MENU ) {
+        if ( gameStateParameter == gameStates.LEVEL_SELECTION_SUB_MENU ) {
             this.gameCurrentState = gameStateParameter;
             mainMenuState = new LevelSelectionState(this);
             graphicsPanel.changeGameStates(mainMenuState);
             levelSelectFlag = true;
         }
-        if (gameStateParameter == gameStates.NEXT ) {
+        if ( gameStateParameter == gameStates.NEXT ) {
             if ( playerSelectFlag == true ) {
                 playerDinoNumber = mainMenuState.incrementIndexCharacterDemo() + 1;
             } else if ( levelSelectFlag == true ) {
                 levelSelected = mainMenuState.incrementIndexLevelNumber() + 1;
             }
         }
-        if (gameStateParameter == gameStates.PERV ) {
+        if ( gameStateParameter == gameStates.PERV ) {
             if ( playerSelectFlag == true ) {
                 playerDinoNumber = mainMenuState.decrementIndexCharacterDemo() + 1;
             } else if ( levelSelectFlag == true ) {
                 levelSelected = mainMenuState.decrementIndexLevelNumber() + 1;
             }        
         }
-        if (gameStateParameter == gameStates.RESTART ) {
+        if ( gameStateParameter == gameStates.RESTART ) {
             this.gameCurrentState = gameStates.IN_LEVEL;
             inLevelState = new InLevelState(this, playerDinoNumber, levelSelected);
             graphicsPanel.changeGameStates(inLevelState);
         }
-        else if (gameStateParameter == gameStates.IN_LEVEL ) {
+        else if ( gameStateParameter == gameStates.IN_LEVEL ) {
             // Here, once the game state is changed to in Level state (the state, where player is playing) in game class,
             // then we change game states in graphicsPanel to render the level state (rendering levels,players,...) .
             this.gameCurrentState = gameStateParameter;
             inLevelState = new InLevelState(this, playerDinoNumber, levelSelected);
             graphicsPanel.changeGameStates(inLevelState);
-        } else if (gameStateParameter == gameStates.QUIT) {
+        } else if ( gameStateParameter == gameStates.QUIT) {
             // if the current state of game is changed to be quit, then terminate the program
             this.gameCurrentState = gameStateParameter;
             System.exit(0);
@@ -124,13 +123,13 @@ public class Game implements Runnable {
     @Override
     public void run() {
         while(true) {
-            if (gameCurrentState == gameStates.IN_MENU) {
+            if ( gameCurrentState == gameStates.IN_MENU ) {
                 // If the current state of game is in main menu state,
                 // then use the update method of inLevelState
                 // TODO writing comments here
                 mainMenuState.update();
             }
-            else if (gameCurrentState == gameStates.IN_LEVEL ) {
+            else if ( gameCurrentState == gameStates.IN_LEVEL ) {
                 // If the current state of game is in playing state,
                 // then use the update method of inLevelState
                 // where it updates the player, ...
--- src/main/java/group7/levels/Level2.java
+++ src/main/java/group7/levels/Level2.java
@@ -7,5 +7,15 @@ import java.awt.Graphics;
 import java.awt.Color;
 
 public class Level2 extends Level {
-    
+    // load/get info on level: enemy, keys, eggs, & potions spawn positions???
+    /*
+     *  KEY LOCATIONS:
+     *       K1( 16, 4 ), K2( 3, 14 ), K3( 14, 16 )
+     *  HEALTH POTION:
+     *       HP1( 9, 12 ), HP2( 17, 8 )
+     *  SPEED POTIION:
+     *       SP1( 3, 12 ), SP2( 14, 13 )
+     *  EGG LOCATIONS:
+     *       E1( 2, 16 ), E2( 7, 3 ), E3( 15, 12 )
+    */  
 }
